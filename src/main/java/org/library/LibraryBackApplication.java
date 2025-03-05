f7f19baa1f1a794ff1d0b2242da423cf08ea7b8a
Breynner Ariza <breynner.fabian.ariza.florez@gmail.com>
2025-03-05 10:15:25 -0500

    Primer commit: Creación de la estructura del proyecto

---
+++ src/main/java/org/library/LibraryBackApplication.java
@@ -0,0 +1,13 @@
+package org.library;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LibraryBackApplication.class, args);
+    }
+
+}
